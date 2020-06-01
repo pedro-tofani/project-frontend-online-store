f3a9b25d87fa13ea171c47c2844bf97609f6ccfe
phtf92 <phtf92@gmail.com>
2020-06-01 12:59:54 -0300

    Updates

---
+++ precache-manifest.ebe513a52488183c4b9aa4c5aca0fdcd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c58676bf8d855b34f191fd13079f4d9",
+    "url": "/project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "ac2ad85586a88d07a566",
+    "url": "/project-frontend-online-store/static/css/main.35f33061.chunk.css"
+  },
+  {
+    "revision": "7a301ff506f1b0c3f46f",
+    "url": "/project-frontend-online-store/static/js/2.0f4adffa.chunk.js"
+  },
+  {
+    "revision": "ac2ad85586a88d07a566",
+    "url": "/project-frontend-online-store/static/js/main.4ffb4796.chunk.js"
+  },
+  {
+    "revision": "9f8dbdb9f1fcadcab5fa",
+    "url": "/project-frontend-online-store/static/js/runtime-main.a9148f17.js"
+  },
+  {
+    "revision": "84ae43ba1d85b8b9dbe778c6fb1548db",
+    "url": "/project-frontend-online-store/static/media/back-arrow.84ae43ba.png"
+  },
+  {
+    "revision": "5e06a009d0d4f3b2d956d07f7eaa3a77",
+    "url": "/project-frontend-online-store/static/media/cartao-credito.5e06a009.jpeg"
+  },
+  {
+    "revision": "85043b2362fe27e3876f262e5cc5a52c",
+    "url": "/project-frontend-online-store/static/media/codigo-barras.85043b23.jpeg"
+  },
+  {
+    "revision": "efe3f88381f935466bd26cdee71e85d8",
+    "url": "/project-frontend-online-store/static/media/graystar.efe3f883.png"
+  },
+  {
+    "revision": "aa9925ad75ebd634a562304eac33712d",
+    "url": "/project-frontend-online-store/static/media/img_290616.aa9925ad.png"
+  },
+  {
+    "revision": "ec498b877495146831f16b704ff8dc7c",
+    "url": "/project-frontend-online-store/static/media/plus.ec498b87.png"
+  }
+]);
